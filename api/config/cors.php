6304a19e99a0efd22204e783387f5891b4e3b72f
Usama Aslam <usamaaslam739@gmail.com>
2025-08-14 12:10:46 +0500

    Deployment Isues fixed

--- api/config/cors.php
+++ api/config/cors.php
@@ -7,7 +7,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => 
-    [ 'http://159.65.152.115/taskhorse'  ],
+    [ 'http://139.59.32.89:3000'  ],
 
     'allowed_origins_patterns' => [],
 
