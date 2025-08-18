138b42ba38f4fc5bc6b373566f65d2e014920b14
Usama Aslam <usamaaslam739@gmail.com>
2025-08-18 13:01:27 +0500

    Fixing Pipeline

--- api/app/Jobs/UploadTaskFileJob.php
+++ api/app/Jobs/UploadTaskFileJob.php
@@ -9,6 +9,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Exceptions;
 
 class UploadTaskFileJob implements ShouldQueue
 {
--- api/routes/web.php
+++ api/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TaskCommentController;
